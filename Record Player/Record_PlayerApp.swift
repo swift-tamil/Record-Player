20b3720ac4a4009f5a038887fdfc64fe1c29c7e7
MohamedGoush <0x1110111@gmail.com>
2023-10-29 15:40:39 +0530

    Initial Commit

---
+++ Record Player/Record_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Record_PlayerApp.swift
+//  Record Player
+//
+//  Created by python on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Record_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
